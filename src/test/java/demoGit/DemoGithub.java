004918b13754a749d1912e6d6a034dc5de94fbe9
User <User@DESKTOP-P309ICG>
2022-01-24 18:30:32 +0530

    user1 first cummit

---
+++ src/test/java/demoGit/DemoGithub.java
@@ -0,0 +1,15 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class DemoGithub {
+	
+	
+	@Test
+	
+	public void demoGitTest(){
+		
+		System.out.println("user 1 first cummit");
+	}
+
+}
